c269a89089e728f359da0b7cf3b2f9e8a3e2d734
yudonglin <yoshino1347716570@gmail.com>
2020-07-06 11:50:58 -0700

    优化

--- vc_ShopEditor.py
+++ vc_ShopEditor.py
@@ -10,6 +10,8 @@ if not os.path.exists("menu"):
 cutLineStartID = 18
 cutLineEndID = 26
 
+ColorsIdList = [2, 3, 4, 5, 6, 7,  9, 'a', 'b', 'c', 'd', 'e', 'f']
+
 #读取config里的配置文件
 allShopFiles = glob("config/*.yaml")
 allShopData = {}
@@ -24,7 +26,7 @@ with open("menu/shop_choose.conf","w+",encoding='utf-8') as f:
     f.write('virtualchest {\n  TextTitle = "&6在线超市-请选择商品种类"\n  Rows = 6\n  UpdateIntervalTick = 20\n  AcceptableActionIntervalTick = 20\n')
     
     for key,value in allShopData.items():
-        f.write('  Slot'+str(value["id"]*2)+' = {\n    Item {\n	  Count = 1\n	  ItemType = "'+value["ItemIcon"]+'"\n	  UnsafeDamage = 0\n      DisplayName = "&'+value["Color"]+value["MenuName"]+'"\n	}\n	PrimaryAction {Command = "vc o '+value["file_name"]+'"}\n  }\n')
+        f.write('  Slot'+str(value["id"]*2)+' = {\n    Item {\n	  Count = 1\n	  ItemType = "'+value["ItemIcon"]+'"\n	  UnsafeDamage = 0\n      DisplayName = "&'+ str(ColorsIdList[value["id"]])+value["MenuName"]+'"\n	}\n	PrimaryAction {Command = "vc o '+value["file_name"]+'"}\n  }\n')
     f.write('  Slot53 =  {\n    Item {\n	  Count = 1\n	  ItemType = "minecraft:redstone"\n	  UnsafeDamage = 0\n      DisplayName = "&f返回"\n      ItemLore = ["&7点击此处返回上一个菜单"]\n    }\n	PrimaryAction {Command = "vc o main"}\n	KeepInventoryOpen = true\n  }\n}')
 
 for key,DATA in allShopData.items():
@@ -84,7 +86,7 @@ for key,DATA in allShopData.items():
         #上一页的按钮
         if DATA["id"]-1 in allShopData:
             f.write('  Slot45 = {\n    Item {\n      Count = 1\n      ')
-            f.write('ItemType = "{2}"\n      UnsafeDamage = 0\n      DisplayName = "&{0}上一页:{1}"\n    '.format(allShopData[DATA["id"]-1]["Color"],allShopData[DATA["id"]-1]["MenuName"],allShopData[DATA["id"]-1]["ItemIcon"]))
+            f.write('ItemType = "{2}"\n      UnsafeDamage = 0\n      DisplayName = "&{0}上一页:{1}"\n    '.format(ColorsIdList[DATA["id"]-1],allShopData[DATA["id"]-1]["MenuName"],allShopData[DATA["id"]-1]["ItemIcon"]))
             f.write('}\n    PrimaryAction {Command = "vc o '+allShopData[DATA["id"]-1]["file_name"]+'"}\n    KeepInventoryOpen = true\n  }\n')
         else:
             f.write('  Slot45 = {\n    Item {\n      Count = 1\n      ItemType = "minecraft:barrier"\n      UnsafeDamage = 0\n      DisplayName = "&c上一页:没有了"\n    }\n    PrimaryAction {Command = "vc o shop_choose"}\n    KeepInventoryOpen = true\n  }\n')
@@ -93,6 +95,6 @@ for key,DATA in allShopData.items():
         #下一页的按钮
         if DATA["id"]+1 in allShopData:
             f.write('  Slot53 = {\n    Item {\n      Count = 1\n      ')
-            f.write('ItemType = "{2}"\n      UnsafeDamage = 0\n      DisplayName = "&{0}下一页:{1}"\n    '.format(allShopData[DATA["id"]+1]["Color"],allShopData[DATA["id"]+1]["MenuName"],allShopData[DATA["id"]+1]["ItemIcon"]))
+            f.write('ItemType = "{2}"\n      UnsafeDamage = 0\n      DisplayName = "&{0}下一页:{1}"\n    '.format(ColorsIdList[DATA["id"]+1],allShopData[DATA["id"]+1]["MenuName"],allShopData[DATA["id"]+1]["ItemIcon"]))
             f.write('}\n    PrimaryAction {Command = "vc o '+allShopData[DATA["id"]+1]["file_name"]+'"}\n    KeepInventoryOpen = true\n  }\n')
         f.write('}')
